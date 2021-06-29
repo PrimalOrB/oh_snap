84b78762cd00aeca3d0e46966231f71f43e8e1ff
Andrew Ogilvie <primalorb@gmail.com>
2021-06-29 19:46:17 -0400

    add about page and nav

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import coverImage from '../../assets/cover/cover-image.jpg'
+
+const About = () => {
+    return (
+        <section>
+            <h1 className="my-5">Who am I?</h1>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+
+        </section>
+    )
+}
+
+export default About
