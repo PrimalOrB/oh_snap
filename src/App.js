c919be61ba414989a0e5a8d3afd51a92a486dd94
Andrew Ogilvie <primalorb@gmail.com>
2021-06-29 21:11:45 -0400

    add photo gallery and tests

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       />
       <main>
         <div>
-          <Gallery />
+          <Gallery currentCategory={ currentCategory }/>
           <About />
         </div>
       </main>
